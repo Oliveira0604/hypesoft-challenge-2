953bc14c11fd1b9b53de03603a721c6c2ca0cedc
oliveira <oliveiranathan853@gmail.com>
2026-03-19 06:53:59 -0300

    feat(domain): add IUserRepository

---
+++ Backend/src/Hypesoft.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,16 @@
+using Hypesoft.Domain.Entities;
+
+namespace Hypesoft.Domain.Interfaces;
+
+public interface IUserRepository
+{
+    Task<IEnumerable<User>> GetAllUsers();
+    Task<User?> GetUserById(string id);
+    Task<User?> GetUserByName(string name);
+    Task AddUserAsync(User user);
+    Task UpdateUserNameAsync(User user);
+    Task UpdateUserEmailAsync(User user);
+    Task UpdateUserCellphoneAsync(User user);
+    Task UpdateUserPasswordAsync(User user);
+    Task DeleteUserAsync(string id);
+}
